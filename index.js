8462d751f5a6a78f7a07a8f44b78a6857e2cdf2b
mamun-prodhan <mamunpr9@gmail.com>
2026-06-04 08:15:56 +0600

    Implement project

---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const fs = require("fs");
+
+const inputArguments = process.argv.slice(2);
+
+const text = inputArguments.join(" ");
+const timestamp = new Date().toISOString();
+
+const message = `${text} ${timestamp}\n`;
+
+if (!message) {
+  console.log("Please provide a message to log");
+  console.log("Example: node index.js Hello World");
+  process.exit(1);
+}
+
+const filePath = path.join(__dirname, "log.txt");
+
+fs.appendFile(filePath, message, { encoding: "utf-8" }, () => {
+  console.log("Your log added successfully");
+});
+
+console.log(filePath);
